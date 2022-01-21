966f67565d9193858f4c62de10be618a59085470
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2022-01-21 22:17:30 +0300

    2

---
+++ Peergrade005/Peergrade005/Class1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using Color = System.Drawing.Color;
+using Pen = System.Drawing.Pen;
+using Point = System.Drawing.Point;
+
+
+namespace Peergrade005
+{
+    class Fractal
+    {
+        private double lenghtOfSegment;
+        private ushort depthOfRecursion;
+        protected Graphics Graphics;
+
+        private Color startColor;
+        private Color endColor;
+        private Color[] colorsGradient;
+
+        public double LengthOfSegment
+        {
+            get => lenghtOfSegment;
+            private set => lenghtOfSegment = value;
+        }
+
+        public ushort RecursionDepth
+        {
+            get => depthOfRecursion;
+            set
+            {
+                depthOfRecursion = value;
+                CreateNewGradient(RecursionDepth);
+            }
+        }
+
+        public Bitmap FractalBitmap { get; set; }
+
+        public Fractal(double segmentLength, ushort recursionDepth, Color startColor, Color endColor)
+        {
+            LengthOfSegment = segmentLength;
+            RecursionDepth = recursionDepth;
+            this.startColor = startColor;
+            this.endColor = endColor;
+            CreateNewGradient(RecursionDepth);
+        }
+
+        protected void CreateNewGradient(int recursionDepth)
+        {
+            colorsGradient = new Color[recursionDepth];
+
+            if (RecursionDepth == 1)
+            {
+                colorsGradient[0] = startColor;
+                return;
+            }
+
+            for (int i = 0; i < recursionDepth; i++)
+            {
+                colorsGradient[i] = Color.FromArgb(
+                    endColor.R + (int)((startColor.R - endColor.R) * i / (double)recursionDepth),
+                    endColor.G + (int)((startColor.G - endColor.G) * i / (double)recursionDepth),
+                    endColor.B + (int)((startColor.B - endColor.B) * i / (double)recursionDepth));
+            }
+        }
+
+        public virtual void CreateFractal(PointF[] points, int recursionLevel)
+        {
+            Graphics = Graphics.FromImage(FractalBitmap);
+
+            if (this is Tree)
+            {
+                Pen nowPen = new Pen(colorsGradient[recursionLevel - 1], 2f);
+                Graphics.DrawLine(nowPen, points[0], points[1]);
+            }
+            else
+            {
+                RectangleF rectangle = new RectangleF(points[0],
+                    new SizeF(points[1].X - points[0].X, points[1].Y - points[0].Y)
+                    );
+                SolidBrush solidBrush = new SolidBrush(colorsGradient[recursionLevel - 1]);
+                Graphics.FillRectangle(solidBrush, rectangle);
+            }
+        }
+    }
+
+    class Tree:Fractal
+    {
+        private double nowAngle;
+        private double firstAngle;
+        private double secondAngle;
+
+        private PointF startPoint;
+        private PointF endPoint;
+
+        private double Coefficient { get; set; }
+
+        private double RightAngle
+        {
+            get => firstAngle;
+            set => firstAngle = value * Math.PI / 180;
+        }
+
+        private double LeftAngle
+        {
+            get => secondAngle;
+            set => secondAngle = value * Math.PI / 180;
+        }
+
+
+        public Tree(float segmentLength, ushort recursionDepth, Color startColor, Color endColor,
+            double rightAngle, double leftAngle, double coefficient) : base(segmentLength, recursionDepth, startColor, endColor)
+        {
+            RightAngle = rightAngle;
+            LeftAngle = leftAngle;
+            Coefficient = coefficient;
+        }
+
+
+        public override void CreateFractal(PointF[] points, int recursionLevel)
+        {
+            if (recursionLevel == 0) return;
+            base.CreateFractal(points, recursionLevel);
+            if (recursionLevel == RecursionDepth)
+            {
+                startPoint = new PointF(-1, -1);
+                endPoint = new PointF(-1, -1);
+            }
+            foreach (PointF point in points)
+            {
+                if (point.X < startPoint.X || startPoint.X == -1) startPoint.X = point.X;
+                if (point.X > endPoint.X || endPoint.X == -1) endPoint.X = point.X;
+                if (point.Y < startPoint.Y || startPoint.Y == -1) startPoint.Y = point.Y;
+                if (point.Y > endPoint.Y || endPoint.Y == -1) endPoint.Y = point.Y;
+            }
+            points[0] = points[1];
+            CreateOneSide(points[1],
+                LengthOfSegment * Math.Pow(Coefficient, RecursionDepth - recursionLevel + 1), LeftAngle, 1, recursionLevel);
+            CreateOneSide(points[1],
+                LengthOfSegment * Math.Pow(Coefficient, RecursionDepth - recursionLevel + 1), RightAngle, -1, recursionLevel);
+        }
+
+
+        private void CreateOneSide(PointF startPoint, double segmentLength, double angle, int direction, int recursionLevel)
+        {
+            nowAngle += angle * direction;
+
+            CreateFractal(
+                new PointF[] { startPoint, new PointF(
+                    (float)(startPoint.X - direction * (segmentLength * Math.Sin(nowAngle * direction))),
+                    (float)(startPoint.Y - (segmentLength * Math.Cos(nowAngle * direction)))) },
+                recursionLevel - 1);
+
+            nowAngle -= angle * direction;
+        }
+
+    }
+}
+
--- Peergrade005/Peergrade005/MainWindow.xaml.cs
+++ Peergrade005/Peergrade005/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Color = System.Drawing.Color;
+using Size = System.Windows.Size;
 
 namespace Peergrade005
 {
@@ -22,9 +26,20 @@ namespace Peergrade005
     {
         private readonly string[] typesFractal = { "FractalTree", "KochCurved", "Carpet", "Triangle", "CantorSet" };
         private ushort nowFractal = 0;
+        private (int, int, int) startRGB = (0, 0, 0);
+        private (int, int, int) endRGB = (0, 0, 0);
+        private Fractal fractal;
+
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowKernel.MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            MainWindowKernel.MaxWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            MainWindowKernel.MinHeight = System.Windows.SystemParameters.PrimaryScreenHeight / 2;
+            MainWindowKernel.MinWidth = System.Windows.SystemParameters.PrimaryScreenWidth / 2;
+
+            ScrollViewer.ScrollToHorizontalOffset(System.Windows.SystemParameters.PrimaryScreenWidth / 4);
+            ScrollViewer.ScrollToVerticalOffset(System.Windows.SystemParameters.PrimaryScreenHeight / 4);
         }
 
         private void SetVisibilityMenu(bool fractalTreeVisible, bool kantorSetVisible)
@@ -85,8 +100,153 @@ namespace Peergrade005
         private void ZoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             this.ZoomTextBlock.Text = $"Масштаб: x{(int)(ZoomSlider.Value)}";
-            this.MainImage.RenderTransform =
+            this.MainImage.LayoutTransform =
                 new ScaleTransform((double) (int) (ZoomSlider.Value), (double) (int) (ZoomSlider.Value));
+        }
+
+        private void SectionLengthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.SectionLengthText.Text = $"Длина отрезка: {(int) SectionLengthSlider.Value}";
+        }
+
+        private void RecursionDeepSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.RecursionDeepText.Text = $"Глубина рекурсии: {(int)RecursionDeepSlider.Value}";
+        }
+
+        private void StartColourRSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            startRGB.Item1 = (int) StartColourRSlider.Value;
+            this.StartColourText.Text = $"({startRGB.Item1},{startRGB.Item2},{startRGB.Item3})";
+        }
+
+        private void StartColourGSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            startRGB.Item2 = (int)StartColourGSlider.Value;
+            this.StartColourText.Text = $"({startRGB.Item1},{startRGB.Item2},{startRGB.Item3})";
+        }
+
+        private void StartColourBSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            startRGB.Item3 = (int)StartColourBSlider.Value;
+            this.StartColourText.Text = $"({startRGB.Item1},{startRGB.Item2},{startRGB.Item3})";
+        }
+
+        private void EndColourRSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            endRGB.Item1 = (int)EndColourRSlider.Value;
+            this.EndColourText.Text = $"({endRGB.Item1},{endRGB.Item2},{endRGB.Item3})";
+        }
+
+        private void EndColourGSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            endRGB.Item2 = (int)EndColourGSlider.Value;
+            this.EndColourText.Text = $"({endRGB.Item1},{endRGB.Item2},{endRGB.Item3})";
+        }
+
+        private void EndColourBSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            endRGB.Item3 = (int)EndColourBSlider.Value;
+            this.EndColourText.Text = $"({endRGB.Item1},{endRGB.Item2},{endRGB.Item3})";
+        }
+
+        private void RelationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.RelationText.Text = $"Отношение длин: {((int)RelationSlider.Value)/(double)1000}";
+        }
+
+        private void FirstAngleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.FirstAngleText.Text = $"Первый угол наклона: {(int)FirstAngleSlider.Value}";
+        }
+
+        private void SecondAngleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.SecondAngleText.Text = $"Второй угол наклона: {(int)SecondAngleSlider.Value}";
+        }
+
+        private void SideSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.SideText.Text = $"Сторона прямоугольника: {(int)SideSlider.Value}";
+        }
+
+        private void DistanceSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.DistanceText.Text = $"Расстояние: {(int)DistanceSlider.Value}";
+        }
+
+        private void CreateFractalButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (nowFractal)
+            {
+                case 0:
+                    fractal = new Tree(
+                        (int) SectionLengthSlider.Value,
+                        (ushort)RecursionDeepSlider.Value,
+                        Color.FromArgb(startRGB.Item1, startRGB.Item2, startRGB.Item3),
+                        Color.FromArgb(endRGB.Item1, endRGB.Item2, endRGB.Item3),
+                        (int)FirstAngleSlider.Value,
+                        (int)SecondAngleSlider.Value,
+                        ((int)RelationSlider.Value) / (double)1000);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+            DrawFractal();
+
+            ScrollViewer.ScrollToHorizontalOffset(System.Windows.SystemParameters.PrimaryScreenWidth / 4);
+            ScrollViewer.ScrollToVerticalOffset(System.Windows.SystemParameters.PrimaryScreenHeight / 4);
+        }
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        private void DrawFractal()
+        {
+
+            try
+            {
+                fractal.FractalBitmap = new Bitmap((int)MainWindowKernel.MaxWidth, (int)MainWindowKernel.MaxHeight);
+                fractal.CreateFractal(GetStartPoints(), fractal.RecursionDepth);
+                MainImage.Source = ImageSourceFromBitmap(fractal.FractalBitmap);
+                fractal.FractalBitmap.Save("ddddddd.png");
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Ошибка {e}");
+            }
+
+        }
+
+        private PointF[] GetStartPoints()
+        {
+            PointF[] points = new PointF[0];
+            float centerX = (float)MainWindowKernel.MaxWidth / 2f;
+            float centerY = (float)MainWindowKernel.MaxHeight / 2f;
+
+            if (fractal is Tree)
+            {
+                points = new PointF[] {
+                    new PointF(centerX, centerY + (float)fractal.LengthOfSegment),
+                    new PointF(centerX, centerY)
+                };
+            }
+            return points;
+        }
+
+        private void MainWindowKernel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
         }
     }
 }
